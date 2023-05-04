c526fbca3a303a9f7b89182f3fa43911612f44b5
perezmotajavier <gabyperezmota@icloud.com>
2023-05-05 01:07:56 +0200

    [FTR]CreativeChessAI

---
+++ README.md
@@ -0,0 +1,1 @@
+# A program that uses neural networks to generate interesting and unusual chess variants. The program evaluates the quality of the generated variants according to the complexity and the strategic interest.
---
+++ data_neural_training.py
@@ -0,0 +1,42 @@
+# By PEREZ MOTA Javier
+
+# Preparing data for neural network training
+import mysql.connector
+import chess.pgn
+from config import db_config
+
+# To connect to the database
+creativechessai_db = mysql.connector.connect(**db_config)
+
+# Open the PGN file
+pgn = open('ChessDB/Databases.pgn')
+
+# Browse through the games in the PGN file one by one
+while True:
+    # Read a game
+    game = chess.pgn.read_game(pgn)
+    if game is None:
+        break
+    # Initialize the board to the starting position of the game
+    board = game.board()
+
+    # Browse through the moves of the game
+    for move in game.mainline_moves():
+
+        # Check if the current position is an opening position
+        if board.is_valid() and board.ply() <= 10:
+            # Add the opening position to the database
+            position = board.fen()
+            cursor = creativechessai_db.cursor()
+            sql = "INSERT INTO openings (position) VALUES (%s)"
+            val = (position,)
+            cursor.execute(sql, val)
+            creativechessai_db.commit()
+
+        # Play the move on the board
+        board.push(move)
+
+# Close the files and the database connection
+pgn.close()
+creativechessai_db.close()
+print("Le script est terminé.")
